65e070a16de8b4a1f1501a49c264c83598a1c1ae
Bradley Mubenga <mubengabradley@gmail.com>
2021-08-14 02:57:20 +0200

    Depploy Fixes

---
+++ pages/navigator.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function navigator() {
+    return (
+        <div>
+            <h1>Hello World! I Will Have All The Links!</h1>
+        </div>
+    )
+}
